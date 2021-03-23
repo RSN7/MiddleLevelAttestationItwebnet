cafa60b5546fb3e0e44133b89a0d2188f5dff3c5
r.sapozhnikov <r.sapozhnikov@www.itwebnet.ru>
2021-03-24 00:41:40 +0300

    Первое задание

---
+++ App1/Figure.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1
+{
+    public class Figure
+    {
+        public double Perimeter { get; }
+        public string TypeFigure { get; }
+        public Figure(Point point1, Point point2, Point point3)
+        {
+            double firstLine = calculateLine(point1, point2);
+            double twoLine = calculateLine(point2, point3);
+            double threeLine = calculateLine(point3, point1);
+
+            Perimeter = сalculatePerimeter(firstLine, twoLine, threeLine);
+            TypeFigure = "Треугольник";
+
+        }
+
+        public Figure(Point point1, Point point2, Point point3, Point point4)
+        {
+
+            double firstLine = calculateLine(point1, point2);
+            double twoLine = calculateLine(point2, point3);
+            double threeLine = calculateLine(point3, point4);
+            double fourLine = calculateLine(point4, point1);
+
+            Perimeter = сalculatePerimeter(firstLine, twoLine, threeLine, fourLine);
+            TypeFigure = "Четырехугольник";
+        }
+
+        public Figure(Point point1, Point point2, Point point3, Point point4, Point point5)
+        {
+            double firstLine = calculateLine(point1, point2);
+            double twoLine = calculateLine(point2, point3);
+            double threeLine = calculateLine(point3, point4);
+            double fourLine = calculateLine(point4, point5);
+            double fiveLine = calculateLine(point5, point1);
+           
+            Perimeter = сalculatePerimeter(firstLine, twoLine, threeLine, fourLine, fiveLine);
+            TypeFigure = "Пятиугольник";
+
+        }
+
+        //метод для вычисления периметра
+        private double сalculatePerimeter(params double[] lines) 
+        {
+            double result = 0;
+            for (int i = 0; i<lines.Length; i++)
+            {
+                result += lines[i];
+            }
+            return Math.Round(result, 2);
+        }
+
+        //метод для вычисления длины стороны
+        private double calculateLine(Point firstPoint, Point twoPoint)
+        {
+            double result = Math.Sqrt((twoPoint.X - firstPoint.X) * (twoPoint.X - firstPoint.X) +
+                                         (twoPoint.Y - firstPoint.Y) * (twoPoint.Y - firstPoint.Y));
+            return Math.Round(result, 2);
+        }
+    }
+}
---
+++ App1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace App1
+{
+    /*
+        1.	Создать классы Point и Figure. Класс Point должен содержать два
+целочисленных поля. Создать конструктор, в теле которого проинициализируйте поля значениями аргументов. Класс Figure должен содержать конструкторы, которые принимают от 3-х до 5-ти аргументов типа Point. Создать два метода: 
+●	Для расчета длины стороны многоугольника; 
+●	Для расчета периметра многоугольника.
+Написать программу, которая выводит на экран название и периметр многоугольника.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point1 = new Point()
+            {
+                X = 1,
+                Y = 1
+            };
+
+            Point point2 = new Point()
+            {
+                X = 1,
+                Y = 3
+            };
+
+            Point point3 = new Point()
+            {
+                X = 2,
+                Y = 1
+            };
+
+            Point point4 = new Point()
+            {
+                X = 3,
+                Y = 4
+            };
+
+            Point point5 = new Point()
+            {
+                X = 2,
+                Y = 1
+            };
+
+            Figure triangle = new Figure(point1, point2, point3);
+            Console.WriteLine("Заданы точки для первой фигуры:");
+            Console.WriteLine($"Первая: {point1.X};{point1.Y}");
+            Console.WriteLine($"Вторая: {point2.X};{point2.Y}");
+            Console.WriteLine($"Третья: {point3.X};{point3.Y}");
+
+            Console.WriteLine($"Первая фигура - {triangle.TypeFigure} ее периметр: {triangle.Perimeter}");
+
+            Figure quadrilateral = new Figure(point1, point2, point3, point4);
+            Console.WriteLine("Заданы точки для второй фигуры:");
+            Console.WriteLine($"Первая: {point1.X};{point1.Y}");
+            Console.WriteLine($"Вторая: {point2.X};{point2.Y}");
+            Console.WriteLine($"Третья: {point3.X};{point3.Y}");
+            Console.WriteLine($"Третья: {point4.X};{point4.Y}");
+
+            Console.WriteLine($"Вторая фигура - {quadrilateral.TypeFigure} ее периметр: {quadrilateral.Perimeter}");
+
+            Figure pentagon = new Figure(point1, point2, point3, point4, point5);
+            Console.WriteLine("Заданы точки для третьей фигуры:");
+            Console.WriteLine($"Первая: {point1.X};{point1.Y}");
+            Console.WriteLine($"Вторая: {point2.X};{point2.Y}");
+            Console.WriteLine($"Третья: {point3.X};{point3.Y}");
+            Console.WriteLine($"Третья: {point4.X};{point4.Y}");
+            Console.WriteLine($"Третья: {point5.X};{point5.Y}");
+
+            Console.WriteLine($"Третья фигура - {pentagon.TypeFigure} ее периметр: {pentagon.Perimeter}");
+        }
+    }
+}
