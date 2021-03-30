164881d199f97b3568691077caaebb6b314f5614
r.sapozhnikov <r.sapozhnikov@www.itwebnet.ru>
2021-03-30 23:40:49 +0300

    Выполнил второе задание

---
+++ App2/DOCHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    public class DOCHandler : AbstractHandler 
+    {
+        public DOCHandler(string NameFile) : base(NameFile) 
+        { 
+        }
+        public override void Open()
+        {
+            Console.WriteLine($"Открыт файл {File} тип файла: DOC");
+        }
+
+        public override void Create()
+        {
+            Console.WriteLine($"Создан файл {File} тип файла: DOC");
+        }
+
+        public override void Change()
+        {
+            Console.WriteLine($"Изменен файл {File} тип файла: DOC");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine($"Сохранен файл {File} тип файла: DOC");
+        }
+    }
+}
---
+++ App2/TXTHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    public class TXTHandler : AbstractHandler
+    {
+        public TXTHandler(string NameFile) : base(NameFile)
+        {
+        }
+        public override void Open()
+        {
+            Console.WriteLine($"Открыт файл {File} тип файла: TXT");
+        }
+
+        public override void Create()
+        {
+            Console.WriteLine($"Создан файл {File} тип файла: TXT");
+        }
+
+        public override void Change()
+        {
+            Console.WriteLine($"Изменен файл {File} тип файла: TXT");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine($"Сохранен файл {File} тип файла: TXT");
+        }
+    }
+}
