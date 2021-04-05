3d12d7b02fb61b33245db0e940eb58d2586389cf
r.sapozhnikov <r.sapozhnikov@www.itwebnet.ru>
2021-04-05 23:49:37 +0300

    App6

---
+++ App6/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace App6
+{
+    /*
+     6.	Создайте анонимный метод, который принимает в качестве аргумента 
+        массив делегатов и возвращает среднее арифметическое возвращаемых значений методов сообщенных с делегатами 
+        в массиве. Методы, сообщенные с делегатами из массива, возвращают случайное значение типа int.
+
+     */
+
+    delegate int intDelegate();
+    delegate decimal arithmeticMean(intDelegate[] intDelegates);
+    class Program
+    {
+        private static int intValueReturn()
+        {
+            Random rnd = new Random();
+            int value = rnd.Next(0, 100);
+            return value;
+        }
+        static void Main(string[] args)
+        {
+            intDelegate[] intDelegates = new intDelegate[]
+            {
+                intValueReturn,
+                intValueReturn,
+                intValueReturn,
+                intValueReturn,
+                intValueReturn
+            };
+
+            arithmeticMean am = delegate (intDelegate[] delegates)
+            {
+                if (delegates.Length > 0)
+                {
+                    int sum = 0;
+                    for (int i = 0; i < delegates.Length; i++)
+                    {
+                        sum = sum + delegates[0]();
+                    }
+                    decimal result = sum / delegates.Length;
+                    return Math.Round(result, 2);
+                } else
+                {
+                    return 0;
+                }
+            };
+
+            decimal result = am(intDelegates);
+
+            Console.WriteLine($"Результат выполнения программы: {result}");
+        }
+    }
+}
