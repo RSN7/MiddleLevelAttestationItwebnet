9fe8025f266f71bd501e7b5d4a78015a4eb2e24a
r.sapozhnikov <r.sapozhnikov@www.itwebnet.ru>
2021-04-08 00:00:37 +0300

    App7

---
+++ App7/MyList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App7
+{
+    public class MyList<T> : IMyList<T>
+    {
+
+        private List<T> listObject = new List<T>();
+        public T this[int index]
+        {
+            get
+            {
+                return listObject.ElementAt(index);
+            }
+            set
+            {
+                listObject[index] = value;
+            }
+        }
+        
+        public int CountElement
+        {
+            get
+            {
+                return listObject.Count;
+            }
+        }
+
+        public void Add(T value)
+        {
+            listObject.Add(value);
+        }
+
+        public void Delete(int index) 
+        {
+            listObject.RemoveAt(index);
+        }
+    }
+}
