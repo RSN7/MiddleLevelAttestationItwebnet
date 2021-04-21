67fdeed46a134b02dd26df0aa2839080a8ec939c
r.sapozhnikov <r.sapozhnikov@www.itwebnet.ru>
2021-04-21 15:41:57 +0300

    App8

---
+++ App8/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace App8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartTimer = new System.Windows.Forms.Button();
+            this.PauseTimer = new System.Windows.Forms.Button();
+            this.StopTimer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartTimer
+            // 
+            this.StartTimer.Location = new System.Drawing.Point(136, 147);
+            this.StartTimer.Name = "StartTimer";
+            this.StartTimer.Size = new System.Drawing.Size(75, 23);
+            this.StartTimer.TabIndex = 0;
+            this.StartTimer.Text = "Старт";
+            this.StartTimer.UseVisualStyleBackColor = true;
+            this.StartTimer.Click += new System.EventHandler(this.StartTimer_Click);
+            // 
+            // PauseTimer
+            // 
+            this.PauseTimer.Location = new System.Drawing.Point(136, 182);
+            this.PauseTimer.Name = "PauseTimer";
+            this.PauseTimer.Size = new System.Drawing.Size(75, 23);
+            this.PauseTimer.TabIndex = 1;
+            this.PauseTimer.Text = "Пауза";
+            this.PauseTimer.UseVisualStyleBackColor = true;
+            this.PauseTimer.Click += new System.EventHandler(this.PauseTimer_Click);
+            // 
+            // StopTimer
+            // 
+            this.StopTimer.Location = new System.Drawing.Point(136, 220);
+            this.StopTimer.Name = "StopTimer";
+            this.StopTimer.Size = new System.Drawing.Size(75, 23);
+            this.StopTimer.TabIndex = 2;
+            this.StopTimer.Text = "Стоп";
+            this.StopTimer.UseVisualStyleBackColor = true;
+            this.StopTimer.Click += new System.EventHandler(this.StopTimer_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 316);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StopTimer);
+            this.Controls.Add(this.PauseTimer);
+            this.Controls.Add(this.StartTimer);
+            this.Name = "Form1";
+            this.Text = "App8";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartTimer;
+        private System.Windows.Forms.Button PauseTimer;
+        private System.Windows.Forms.Button StopTimer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ App8/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App8
+{
+    /*
+    Создайте программу секундомер. Требуется выводить показания
+    секундомера в окне. Окно имеет кнопки запуска, останова и сброса секундомера. 
+    Для реализации секундомера используйте события. Нельзя использовать компонент Timer в WindowsForm.
+    https://www.youtube.com/watch?v=6vP9FsWJ2IM
+
+     */
+    public partial class Form1 : Form
+    {
+        private int endSecondTime { get; set; }
+        private static int TimerStart { get; set; }
+        private Thread myThread;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        private void StartTimer_Click(object sender, EventArgs e)
+        {
+            TimerStart = 1;
+            if (myThread == null || TimerStart == 1)
+            {
+                myThread = new Thread(new ThreadStart(
+                () =>
+                {
+                    int  SecondTime = endSecondTime;
+                    while (TimerStart == 1)
+                    {
+                        Thread.Sleep(1000);
+                        SecondTime++;
+
+                        Label1Set($"Значение секундомера: {SecondTime} сек.");
+                    }
+                    if (TimerStart == 2)
+                    {
+                        Label1Set($"Секунодомер приостановлен на значении: {SecondTime} сек.");
+                        endSecondTime = SecondTime;
+                    }
+                    if (TimerStart == 3)
+                    {
+                        Label1Set($"Секунодомер остановлен на значении: {SecondTime} сек.");
+                        endSecondTime = 0;
+                    }
+                }));
+                myThread.IsBackground = false;
+                myThread.Start();
+            }
+        }
+
+        private void Label1Set(string textLabel) 
+        {
+            this.label1.BeginInvoke((MethodInvoker)(() =>
+                            this.label1.Text = textLabel));
+        }
+        private void PauseTimer_Click(object sender, EventArgs e)
+        {
+            TimerStart = 2;
+        }
+
+        private void StopTimer_Click(object sender, EventArgs e)
+        {
+            TimerStart = 3;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            TimerStart = 3;
+        }
+    }
+}
