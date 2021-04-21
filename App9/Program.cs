707fd59bdb67b44368c0207aaa2cea1a584f7ed7
r.sapozhnikov <r.sapozhnikov@www.itwebnet.ru>
2021-04-21 23:39:24 +0300

    App9

---
+++ App9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace App9
+{
+    /*
+     Напишите программу, в которой метод будет вызываться рекурсивно. 
+     Каждый новый вызов метода выполняется в отдельном потоке.
+
+     */
+    class Program
+    {
+        public static void RecursiveThread(int valueThread, int numberThread = 1) 
+        {
+            Console.WriteLine($"Отработал поток № {numberThread}");
+            numberThread++;
+            valueThread--;
+            var myThread = new Thread(new ThreadStart(
+                () =>
+                {
+                    if (valueThread > 0)
+                    {
+                        RecursiveThread(valueThread, numberThread);
+                    }
+                }));
+            myThread.Start();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Рекурсивный вызов 10 потоков:");
+            RecursiveThread(10);
+        }
+    }
+}
